0c1618c693675716ec5001be8cdda450c1e45933
KrisCea <cris.cea@duocuc.cl>
2026-09-22 16:39:10 -0300

    Se implementa react (vite)

---
+++ cv-webpage/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    <footer>
+        <div >
+            <p >&copy; 2026 Cristóbal Cea Guzmán. Todos los derechos reservados.</p>
+        </div>
+    </footer>
+    
+}
+
+export default Footer
---
+++ cv-webpage/src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import './Footer.jsx'
+import Footer from './Footer.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  
+  <Footer>
+    <Footer/>
+  </Footer>
+)
