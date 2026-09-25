dd4da50e3c649608579b625087521cb4a3c84ff3
Kris <cris.cea@duocuc.cl>
2026-09-25 15:37:08 -0300

    Resolucion de problemas (1)

--- cv-webpage/src/main.jsx
+++ cv-webpage/src/main.jsx
@@ -1,11 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Layout } from './core/Layout';
-import { HomePage } from './pages/HomePage';
-import { UserProfile } from './pages/UserProfile';
 import './index.css'
-import App from './pages/App.jsx'
+import App from './pages/App'
 
  
 const router = createBrowserRouter([
@@ -13,8 +10,7 @@ const router = createBrowserRouter([
     path: '/',
     element: <Layout />,
     children: [
-      { index: true, element: <HomePage /> },
-      { path: 'users/:userId', element: <UserProfile /> },
+      { index: true, element: <App/>}
     ],
   },
 ]);
